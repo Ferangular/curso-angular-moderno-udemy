4bd1029b8b32605bac663b1eb6fcb7df9ce916e6
Ferangular <firthunsnpm@gmail.com>
2025-01-01 15:53:19 +0100

    Sección 9, rxjs interop: - interfaz tosignalOptions - migrando toSignal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { AsyncPipe } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, effect, inject } from '@angular/core';
 import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { CategoryFilterComponent } from '@features/categories/category-filter/category-filter.component';
 import { FooterComponent } from '@layout/footer/footer.component';
 import { HeaderComponent } from '@layout/header/header.component';
 import HeroComponent from '@layout/hero/hero.component';
 import { SpinnerComponent } from '@shared/ui/spinner/spinner.component';
-import { filter } from 'rxjs';
+import { filter, of } from 'rxjs';
 import { CartStateService } from 'src/app/store/cart-state/cart-state.service';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   standalone: true,
@@ -27,8 +28,17 @@ import { CartStateService } from 'src/app/store/cart-state/cart-state.service';
 export class AppComponent {
   currentRoute = '';
   readonly cart$ = inject(CartStateService).cart$;
-
   private readonly _router = inject(Router);
+o$= of(1,2,3,4,5);
+signal = toSignal(this.o$, {
+  // initialValue: 0,
+  requireSync: true,
+  manualCleanup: true, // limpieamos el effect
+  rejectErrors: true,
+  // equal: () => console.log // util para controlar los valores igulalres
+  // injector:
+})
+e = effect(()=> console.log(`Current value: ${this.signal()}`))
 
   constructor() {
     this._router.events
@@ -37,4 +47,6 @@ export class AppComponent {
         this.currentRoute = event.urlAfterRedirects.slice(1);
       });
   }
+
+
 }
